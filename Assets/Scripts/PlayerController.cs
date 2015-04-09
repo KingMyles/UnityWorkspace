ba75e536df3e5e9dc08730403590ed736520aac8
Myles j <Landais.myles@gmail.com>
2015-04-09 18:13:48 -0500

    9/4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,21 +13,38 @@ public class PlayerController : MonoBehaviour
 	public float tilt;
 	public Boundary boundary;
 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	void Update ()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			//GameObject clone = 
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation); //as GameObject;
+			GetComponent<AudioSource>().Play ();
+		}
+	}
+
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rigidbody.velocity = movement * speed;
+		GetComponent<Rigidbody>().velocity = movement * speed;
 
-		rigidbody.position = new Vector3 
+		GetComponent<Rigidbody>().position = new Vector3 
 		(
-			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 
+			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
 			0.0f, 
-			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
 		);
 
-		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 	}
 }
